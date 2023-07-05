37b159937dbf23144f69da8389f0c70131655cba
lucius-César <luciendelgrange5@gmail.com>
2023-07-05 15:40:42 +0200

    deployment

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 const API_KEY = process.env.API_KEY
-console.log(API_KEY)
 router.get(("/movies"), (req, res) => {
-    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${REACT_APP_API_KEY}`)
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`)
     .then(response => response.json())
     .then(data => res.json({movies : data.results}))
 }
